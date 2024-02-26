4dc8f711183a4bd154275f47f11a09773b170c47
Thomas <thomaspmar@protonmail.com>
2024-02-25 17:25:52 -0800

    Added icon

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -8,6 +8,6 @@ public static class ModHelperData
 
     public const string Description = "Gain Knowledge Points when you beat round 100! Gain 1 point by default, or configure it in the mod settings! Works in single-player and co-op.";
 
-    public const string RepoOwner = "Gurrenm4"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoOwner = "Gurrenm3"; // TODO add your github username hero, also in the download url in README.md
     public const string RepoName = "BTD6-GainKnowledgeOnRound100"; // TODO add your repo name here, also in the download url in README.md
 }
