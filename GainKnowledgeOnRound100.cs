e053d722bc3401e3bc87a8352a2b9a3d523b858f
Thomas <thomaspmar@protonmail.com>
2024-02-26 19:55:46 -0800

    You now gain extra knowledge points on round 125 as well.

--- GainKnowledgeOnRound100.cs
+++ GainKnowledgeOnRound100.cs
@@ -7,6 +7,7 @@ using Il2CppAssets.Scripts.Models.Store;
 using Il2CppAssets.Scripts.Unity.UI_New.InGame;
 using Il2CppAssets.Scripts.Unity;
 using BTD_Mod_Helper.Extensions;
+using System.Linq;
 
 [assembly: MelonInfo(typeof(GainKnowledgeOnRound100.GainKnowledgeOnRound100), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
@@ -16,7 +17,7 @@ namespace GainKnowledgeOnRound100;
 public class GainKnowledgeOnRound100 : BloonsTD6Mod
 {
     /// <summary>
-    /// The mod setting that controls how many knowledge points the player earns by beating round 100.
+    /// The mod setting that controls how many knowledge points the player earns by beating one of the reward rounds.
     /// </summary>
     public ModSettingInt knowledgePointsToGain = new ModSettingInt(1)
     {
@@ -26,10 +27,14 @@ public class GainKnowledgeOnRound100 : BloonsTD6Mod
     };
 
     /// <summary>
-    /// The round number that the player earns knowledge points from.
+    /// The round numbers that the player earns knowledge points from.
     /// <br/>Must be 1 less than the desired round number.
     /// </summary>
-    public const int roundNumberToEarnReward = 99;
+    public readonly int[] roundNumbersToEarnReward = new[]
+    {
+        99,
+        124
+    };
 
     private bool isSandboxMode;
 
@@ -53,7 +58,7 @@ public class GainKnowledgeOnRound100 : BloonsTD6Mod
 
     /// <summary>
     /// Runs code at the end of each round. 
-    /// <br/>Will award knowledge points to the player if the round that just ended is 100.
+    /// <br/>Will award knowledge points to the player if the round that just ended is one of the reward rounds.
     /// </summary>
     public override void OnRoundEnd()
     {
@@ -61,7 +66,7 @@ public class GainKnowledgeOnRound100 : BloonsTD6Mod
     }
 
     /// <summary>
-    /// Attempts to add the knowledge points if round 100 was beaten and you are not in sandbox mode.
+    /// Attempts to add the knowledge points if one of the reward rounds was beaten and you are not in sandbox mode.
     /// </summary>
     public void TryAddKnowledgePoints()
     {
@@ -69,8 +74,10 @@ public class GainKnowledgeOnRound100 : BloonsTD6Mod
         if (isSandboxMode)
             return;
 
+        int currentRound = InGame.instance.GetSimulation().GetCurrentRound();
+
         // don't run if it's not the correct round
-        if (!CanEarnRewardThisRound())
+        if (!CanEarnRewardThisRound(currentRound))
             return;
 
         // apply knowledge points.
@@ -78,7 +85,7 @@ public class GainKnowledgeOnRound100 : BloonsTD6Mod
         GiveKnowledgePoints(amountToGain);
 
         // show popup and debug message.
-        NotifyKnowledgePointsGained(amountToGain);
+        NotifyKnowledgePointsGained(amountToGain, currentRound);
     }
 
     /// <summary>
@@ -94,14 +101,12 @@ public class GainKnowledgeOnRound100 : BloonsTD6Mod
     /// Returns whether or not the reward can be earned on the current round.
     /// </summary>
     /// <returns></returns>
-    private bool CanEarnRewardThisRound()
+    private bool CanEarnRewardThisRound(int currentRound)
     {
-        int currentRound = InGame.instance.GetSimulation().GetCurrentRound();
-
-        bool isDesiredRound = currentRound == roundNumberToEarnReward;
+        bool isDesiredRound = roundNumbersToEarnReward.Contains(currentRound);
 
         // can only earn rewards if the player beat the desired round.
-        return isDesiredRound ;
+        return isDesiredRound;
     }
 
     /// <summary>
@@ -118,9 +123,9 @@ public class GainKnowledgeOnRound100 : BloonsTD6Mod
     /// Notifies the player that they have gained knowledge points.
     /// </summary>
     /// <param name="amount">The number of knowledge points that the user gained.</param>
-    private void NotifyKnowledgePointsGained(int amount)
+    private void NotifyKnowledgePointsGained(int amount, int currentRoundNumber)
     {
-        string rewardText = $"Congratulations on beating round {roundNumberToEarnReward + 1}! You gained {amount} more knowledge point(s)!!";
+        string rewardText = $"Congratulations on beating round {currentRoundNumber + 1}! You gained {amount} more knowledge point(s)!!";
         ModHelper.Msg<GainKnowledgeOnRound100>(rewardText);
         Game.instance.GetPopupScreen().ShowOkPopup(rewardText);
     }
